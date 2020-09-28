2aedf28e994f23e6b04cdbf6b623f0e3a23aa8f2
Naufal Ahmad Ramadhan <71258004+naufalsptr@users.noreply.github.com>
2020-09-28 20:29:20 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,26 @@
-
+<?php
+if ($_GET):
+    $bilangan1 = $_GET['bilangan1'];
+    $bilangan2 = $_GET['bilangan2'];
+    $operasi = $_GET['pilihanoperasi'];
+    switch ($operasi) {
+        case 'tambah':
+            $hasil = $bilangan1 + $bilangan2;
+            break;
+        case 'kurang':
+            $hasil = $bilangan1 - $bilangan2;
+            break;
+        case 'kali':
+            $hasil = $bilangan1 * $bilangan2;
+            break;
+        case 'bagi':
+            $hasil = $bilangan1 / $bilangan2;
+            break;
+    }
+ ?>
+    <br><h2> &nbsp;&nbsp;&nbsp;
+    Hasil: <strong><?php echo $hasil ?></strong>
+       </h2>
+    <?php
+        endif; 
+    ?>
