abb33b62cf549f049b0756cbd9e5e763f26cfd1d
akbarhaikalf <71258385+akbarhaikalf@users.noreply.github.com>
2020-09-28 20:50:31 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,25 @@
-
+<h2>
+<?php
+$bilangan1 = $_GET['bilangan1'];
+$bilangan2 = $_GET['bilangan2'];
+$operator = $_GET['operator'];
+echo "Bilangan 1 adalah $bilangan1 <br>";
+echo "Bilangan 2 adalah $bilangan2 <br>";
+if($operator == 1){
+    $hasil = $bilangan1 + $bilangan2;
+    echo "Hasil Pertambahan Adalah $hasil";
+}
+elseif($operator == 2){
+    $hasil = $bilangan1 - $bilangan2;
+    echo "Hasil Pengurangan Adalah $hasil";
+}
+elseif($operator == 3){
+    $hasil = $bilangan1 * $bilangan2;
+    echo "Hasil Perkalian Adalah $hasil";
+}
+elseif($operator == 4){
+    $hasil = $bilangan1 / $bilangan2;
+    echo "Hasil Pembagian Adalah $hasil";
+}
+?>
+</h2>
